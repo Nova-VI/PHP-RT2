09b5638e1e8dcdd084fbde626f8dff6f74a14104
Aymen Chahed <aymen.chahed.13@gmail.com>
2024-03-26 01:14:59 +0100

    Added reusable database connection file Added a reusable database connection file ("Connexion.php") to be used by everyone during the project. This file encapsulates the database connection logic, making it easier for the team to access the database without duplicating code.

---
+++ connexion.php
@@ -0,0 +1,24 @@
+<?php
+class ConnexionBD
+{
+    private static $_dbname = "sql11694152";
+    private static $_user = "sql11694152";
+    private static $_pwd = "bBcm9J2BrC";
+    private static $_host = "sql11.freesqldatabase.com";
+    private static $_bdd = null;
+    private function __construct()
+    {
+        try {
+            self::$_bdd = new PDO("mysql:host=" . self::$_host . ";dbname=" . self::$_dbname . ";charset=utf8", self::$_user, self::$_pwd,    array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8'));
+        } catch (PDOException $e) {
+            die('Erreur : ' . $e->getMessage());
+        }
+    }
+    public static function getInstance()
+    {
+        if (!self::$_bdd) {
+            new ConnexionBD();
+        }
+        return (self::$_bdd);
+    }
+}
