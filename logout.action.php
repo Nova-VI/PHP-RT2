566ad28c15542bf857a93ef09a9d45fa7f426954
Aymen Chahed <aymen.chahed.13@gmail.com>
2024-03-27 23:16:52 +0100

    Added some UI enhancement to logout page

--- logout.action.php
+++ logout.action.php
@@ -1,10 +1,5 @@
 <?php
-
-if (isset($_POST['unsetSession'])) {
-    session_start();
-    session_reset();
-    session_destroy();
-}
-
-
+session_start();
+session_reset();
+session_destroy();
 header("Location: index.php");
--- logout.php
+++ logout.php
@@ -1,5 +1,62 @@
-<h1>Are you sure you want to log out? </h1>
-<form action="logout.action.php" method=post>
-    <button type="submit" name="unsetSession">Yes</button>
-</form>
-<button onclick="setActive(list[0])">No</button>
+<div class="logout-page">
+    <h1 style="font-size: 3em;">Are you sure you want to log out? </h1>
+    <span>
+        <button onclick="setActive(list[0])" name="no">No</button>
+        <button type="submit" name="unsetSession" onclick="window.location.href='logout.action.php'">Yes</button>
+    </span>
+</div>
+<style>
+    .logout-page {
+        padding: 50px;
+        margin: 50px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+    }
+
+    .logout-page button {
+        padding: 10px 20px;
+        margin: 10px;
+        border: 2px solid black;
+        border-radius: 5px;
+        background-color: #f1f1f1;
+        cursor: pointer;
+        flex-grow: 1;
+        margin: 20px;
+        transition: .5s;
+        font-size: 2em;
+    }
+
+    .logout-page button[name="unsetSession"] {
+        border: 2px solid red;
+        color: red;
+    }
+
+    .logout-page button[name="no"]:hover {
+        background-color: black;
+        color: white
+    }
+
+    .logout-page button[name="unsetSession"]:hover {
+        background-color: red;
+        color: white
+    }
+
+    .logout-page button:active {
+        background-color: #ccc;
+    }
+
+    .logout-page h1 {
+        margin-bottom: 20px;
+    }
+
+    .logout-page span {
+        margin: 40px;
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+</style>
