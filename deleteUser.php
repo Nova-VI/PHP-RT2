3791751566279ce482dc892a441693340e03968d
Aymen Chahed <aymen.chahed.13@gmail.com>
2024-03-27 14:44:49 +0100

    Added some imaginary users images to be seen in profile pages and users page for admins. Fixed some minor UI bugs

--- deleteUser.php
+++ deleteUser.php
@@ -1,9 +1,11 @@
 <?php
 $username = $_GET['username'];
-echo $username;
 session_start();
 require 'connexion.php';
 $cnx = ConnexionBD::getInstance();
+$query = "select * from users where username = '$username'";
+$user = $cnx->query($query)->fetch(PDO::FETCH_ASSOC);
+unlink($user['profile_image']);
 $query = "delete from users where username = '$username'";
 $cnx->query($query);
 $query = "select * from users";
